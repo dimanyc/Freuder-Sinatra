d601f91d81d8ec5fa54943dd3c5f5d24cdc894b4
dimanyc <dimitrynazarov@gmail.com>
2014-10-23 20:14:00 -0400

    John helped fix the flash issue, figured out the New User page data pull

--- app.rb
+++ app.rb
@@ -16,18 +16,24 @@ get '/' do
 end
 
 
-post '/' do 
+post '/' do
+
 	@user = User.where(username: params[:username]).first
+	@user_max = User.ids.max
+	@user_range = (0..@user_max)
 
-
-	if @user.password == params[:password]
+		if @user && @user.password == params[:password]
 		session[:user_id] = @user.id
 		flash[:notice] = "User signed in successfully."
 		erb :user,:locals => {:user_id => @user_id}
-	else
-		erb :home, :layout => :main
-		flash[:alert] = "Incorrect Password" #ERROR: loading this flash alert on a blank page, omits the layout.rb for some reason.
+		else
 
+		
+		flash.now[:alert] = "Incorrect Username or Password." 
+		erb :home, :layout => :main
+		#ERROR: loading this flash alert on a blank page, omits the layout.rb for some reason.
+	#yield?
+	#elsif @user.username.where([username == params[:username]]) <=> params[:username] 
 	end	
 end
 
@@ -47,15 +53,17 @@ end
 
 
 get '/sign-up' do
-	erb :new_user_form
+	erb :new_user_form, :layout => :main
 end
 
 post '/sign-up' do
 	new_user = User.new(params)
+	@user = User.where(username: params[:username]).first
 	if(new_user.save)
-		erb :user, :locals => {:user => new_user}
+		erb :user, :locals => {:user => @new_user}
 	else
-		#
+		flash.now[:notice] = "Thanks for registering. Grandpa Sig is excited to see you!" 
+		erb :home, :layout => :main	
 	end
 
 end
