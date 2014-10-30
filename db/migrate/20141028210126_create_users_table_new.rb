f3c3a4fe62e58a703065c2519e22d66d7254fe9d
dimanyc <dimitrynazarov@gmail.com>
2014-10-30 19:10:40 -0400

    v3 fixing the users table migation

--- db/migrate/20141028210126_create_users_table_new.rb
+++ db/migrate/20141028210126_create_users_table_new.rb
@@ -1,13 +1,6 @@
 class CreateUsersTableNew < ActiveRecord::Migration
   def change
-  	  	change_table (:users) do |t|
-  		t.string :fname
-  		t.string :lname 
-  		t.string :username
-  		t.string :password
-  		t.string :email 
-  		t.string :img #path to img file
-  		t.string :bio
+  	  add_column :users, :bio, :string 
   	end
   end
 end
